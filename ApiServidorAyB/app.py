157c15ba72ab6a782935531321b584a7fb011c89
Diego <ultimaster22@gmail.com>
2020-10-06 02:00:23 -0600

    Arreglo del servidor AyB

---
+++ ApiServidorAyB/app.py
@@ -0,0 +1,24 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def ObtenerRam():
+    fs = open('/elements/procs/ram-module','r')
+    info = fs.read()
+    fs.close()  # CIERRO CONEXION
+
+    valores = info.split(',')
+    totalram = int(valores[0])
+    free = int(valores[1])
+    cached = int(valores[2])
+    buffer = int(valores[3])
+
+    total = totalram - ( free + buffer + cached )
+    total = ( total/totalram ) *100
+
+    return '<h1>'+total+'/<h1>'
+
+# PARA CORRER EL ARCHIVO EN LA CONSOLA ES python app.py
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=4000)
