4b3fb8ea4a82bfd719e77e45b59a412643421222
Diego <ultimaster22@gmail.com>
2020-10-07 21:05:49 -0600

    Arreglo api del servidor1

--- Servidor1/app.py
+++ Servidor1/app.py
@@ -13,3 +13,7 @@ def prueba():
     ruta = 'http://{}{}'.format(rutas.routes['ip'],rutas.routes['feature'])
     result = get(ruta)
     return result.json() 
+
+# PARA CORRER EL ARCHIVO EN LA CONSOLA ES python app.py
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=4000)
