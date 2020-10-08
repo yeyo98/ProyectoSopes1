bf6da917211467cff4525fab69c808af811c630e
Diego <ultimaster22@gmail.com>
2020-10-08 01:31:36 -0600

    Agregar api balanceadorNotas

--- Servidor1/app.py
+++ Servidor1/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS, cross_origin
-from requests import get
+from requests import get, post
 
 import rutas
 
@@ -9,13 +9,59 @@ CORS(app)
 
 
 @app.route('/', methods=['GET'])
-def prueba():
+def allDates():
     rutaA = 'http://{}{}'.format(rutas.routes['ipa'],rutas.routes['feature'])
     rutaB = 'http://{}{}'.format(rutas.routes['ipb'],rutas.routes['feature'])
 
-    datosA = get(rutaA)
-    datosB = get(rutaB)
+    datosA = get(rutaA) # DATOS DEL SERVIDOR A
+    datosB = get(rutaB) # DATOS DEL SERVIDOR B
     return jsonify({'serveA':datosA.json(),'serveB':datosB.json()}) 
+
+
+@app.route('/BalanceadorNotas', methods=['POST'])
+def Balanceador():
+    content = request.get_json()
+    autor = content['autor']
+    nota = content['nota']
+
+    rutaA = 'http://{}{}'.format(rutas.routes['ipa'],rutas.routes['feature'])
+    rutaB = 'http://{}{}'.format(rutas.routes['ipb'],rutas.routes['feature'])
+
+    datosA = get(rutaA) # DATOS DEL SERVIDOR A
+    datosB = get(rutaB) # DATOS DEL SERVIDOR B
+
+    ip = rutas.routes['ipb'] if selectServer(datosA,datosB) else rutas.routes['ipa']
+    url = 'http://{}{}'.format( ip , rutas.routes['newnote'] )
+    
+    try:
+        post( url , jsonify({'autor': autor, 'nota': nota}) )
+        return jsonify( {'estado': 200, 'message': 'Se paso la cita correctamente'} )
+    except:
+        return jsonify( {'estado': 400, 'message': 'Hubo un error para pasar la cita'} )
+
+# false->serverA true->serverB
+def selectServer(serverA, serverB):
+    if serverA['estado'] == 400:
+        return True
+    elif serverB['estado'] == 400:
+        return False
+    elif serverA['cantidad'] > serverB['cantidad']:
+        return True
+    elif serverA['cantidad'] < serverB['cantidad']:
+        return False
+    elif serverA['ram'] < serverB['ram']:
+        return False
+    elif serverA['ram'] > serverB['ram']:
+        return True
+    elif serverA['cpu'] < serverB['cpu']:
+        return False
+    elif serverA['cpu'] > serverB['cpu']:
+        return True
+    
+    return False
+    
+
+
 
 # PARA CORRER EL ARCHIVO EN LA CONSOLA ES python app.py
 if __name__ == '__main__':
--- Servidor1/rutas.py
+++ Servidor1/rutas.py
@@ -2,4 +2,5 @@ routes = {
     'ipa': '35.236.53.86',
     'ipb': '34.94.243.77',
     'feature': '/serverFeature',
+    'newnote': '/ingresarCita',
 }
